c36ef14231dec481ba727e79a022f66fb05af54f
김혜진 <khj000217@naver.com>
2026-04-15 14:52:39 +0900

    feat: jest 설치

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,12 +1,12 @@
-import '@testing-library/jest-dom';
+// import '@testing-library/jest-dom';
 
-jest.mock('jotai-tanstack-query', () => ({
-  atomWithQuery: jest.fn(),
-  atomWithMutation: jest.fn(),
-}));
+// jest.mock('jotai-tanstack-query', () => ({
+//   atomWithQuery: jest.fn(),
+//   atomWithMutation: jest.fn(),
+// }));
 
-jest.mock('jotai', () => ({
-  atom: jest.fn(),
-  useAtom: jest.fn(() => [null, jest.fn()]),
-  useSetAtom: jest.fn(() => jest.fn()),
-}));
+// jest.mock('jotai', () => ({
+//   atom: jest.fn(),
+//   useAtom: jest.fn(() => [null, jest.fn()]),
+//   useSetAtom: jest.fn(() => jest.fn()),
+// }));
